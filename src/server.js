5b5c691d29d4eb1bb1d88f5ab0fedb21daae0b79
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-07-20 01:22:08 +0300

    hw-7

--- src/server.js
+++ src/server.js
@@ -2,16 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import env from './utils/env.js';
 import contactsRouter from './routes/contacts.js';
 import authRouter from './routes/auth.js';
 import notFoundHandler from './middlwares/notFoundHandler.js';
 import errorHandler from './middlwares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from '../src/middlwares/swaggerDocs.js';
 
 import Contact from './db/models/Contacts.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const UPLOAD_DIR = path.join(__dirname, process.env.UPLOAD_DIR);
 
 export function setupServer() {
   const app = express();
@@ -35,6 +43,9 @@ export function setupServer() {
   });
 
   app.use(cookieParser());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
